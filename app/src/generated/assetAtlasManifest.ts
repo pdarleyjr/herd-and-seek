c313931f50cf57f177a65d7a71968ccb79e3a22d
pdarleyjr <pdarleyjr@gmail.com>
2026-07-10 20:08:41 -0400

    chore: update generated asset timestamps

--- app/src/generated/assetAtlasManifest.ts
+++ app/src/generated/assetAtlasManifest.ts
@@ -35,7 +35,7 @@ export interface AssetAtlasManifest {
 
 export const ATLAS_MANIFEST: AssetAtlasManifest = {
   "version": 1,
-  "generatedAt": "2026-07-10T23:50:55.929Z",
+  "generatedAt": "2026-07-10T23:54:26.443Z",
   "atlases": [
     {
       "id": "forest@1x",
